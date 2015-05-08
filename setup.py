c1fa6fcc50a41ba834ba492cc037bfd1f4b7ec27
Vladimir Ignatev <ya.na.poche@gmail.com>
2015-05-08 13:17:42 +0400

    packaging of PhantomJS

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ from subprocess import call
 from setuptools import setup
 
 PHANTOMJS_EXEC = 'phantomjs'
+NVM_EXEC = 'nvm'
 
 def cmd_exists(cmd):
     return call("type " + cmd, shell=True, 
         stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 if not cmd_exists(PHANTOMJS_EXEC):
-    npm_result = call(["npm", "install", "-g", "phantomjs"], shell=True, 
+    install_cmd = ["npm", "install", "-g", "phantomjs"]
+    if cmd_exists(NVM_EXEC):
+        install_cmd = ["nvm", "use", "0.10"] + install_cmd
+    npm_result = call(install_cmd, shell=True, 
                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if npm_result != 0:
